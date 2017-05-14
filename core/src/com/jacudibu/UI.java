b18dacafa7d6cae5db4e3b338d19b0c29e45ddab
Jacudibu <jacudibu@gmail.com>
2017-05-14 21:23:42 +0200

    Added a skin and made the UI Scalable as well.

--- core/src/com/jacudibu/UI.java
+++ core/src/com/jacudibu/UI.java
@@ -9,27 +9,39 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /**
  * Created by Stefan Wolf (Jacudibu) on 14.05.2017.
  */
 public class UI implements Screen {
-    public static Stage stage;
+    private Stage stage;
+    private Skin skin;
 
     private Texture addTrackerTexture;
     private Texture addTrackerPressedTexture;
     private Texture addMarkerTexture;
     private Texture addMarkerPressedTexture;
 
+    ImageButton createMarker;
+    ImageButton createTracker;
+
     public UI() {
-        stage = new Stage(new FitViewport(Core.windowWidth, Core.windowHeight));
+        stage = new Stage(new ScreenViewport());
         Core.inputMultiplexer.addProcessor(stage);
+        setupSkin();
+
 
         createUpperButtonRow();
+
+        TextField test = new TextField("", skin);
+        test.setPosition(Core.windowWidth - 10, Core.windowHeight - 10, Align.topRight);
+        stage.addActor(test);
     }
 
     @Override
@@ -45,6 +57,7 @@ public class UI implements Screen {
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
+        updateUIPositions();
     }
 
     @Override
@@ -71,13 +84,16 @@ public class UI implements Screen {
         stage.dispose();
     }
 
+    private void setupSkin() {
+        skin = new Skin(Gdx.files.internal("skins/uiskin.json"));
+    }
+
     private void createUpperButtonRow() {
         addMarkerTexture = new Texture(Gdx.files.internal("addMarker.png"));
         addMarkerPressedTexture = new Texture(Gdx.files.internal("addMarkerPressed.png"));
 
-        ImageButton createMarker = new ImageButton(new TextureRegionDrawable(new TextureRegion(addMarkerTexture)),
+        createMarker = new ImageButton(new TextureRegionDrawable(new TextureRegion(addMarkerTexture)),
                 new TextureRegionDrawable(new TextureRegion(addMarkerPressedTexture)));
-        createMarker.setPosition(10, Core.windowHeight - 10, Align.topLeft);
 
         createMarker.addListener(new ActorGestureListener() {
             public void tap(InputEvent event, float x, float y, int count, int button) {
@@ -90,9 +106,8 @@ public class UI implements Screen {
         addTrackerTexture = new Texture(Gdx.files.internal("addTracker.png"));
         addTrackerPressedTexture = new Texture(Gdx.files.internal("addTrackerPressed.png"));
 
-        ImageButton createTracker = new ImageButton(new TextureRegionDrawable(new TextureRegion(addTrackerTexture)),
+        createTracker = new ImageButton(new TextureRegionDrawable(new TextureRegion(addTrackerTexture)),
                 new TextureRegionDrawable(new TextureRegion(addTrackerPressedTexture)));
-        createTracker.setPosition(20 + 64,Core.windowHeight - 10, Align.topLeft);
 
         createTracker.addListener(new ActorGestureListener() {
             public void tap(InputEvent event, float x, float y, int count, int button) {
@@ -102,5 +117,15 @@ public class UI implements Screen {
         });
 
         stage.addActor(createTracker);
+
+        updateUIPositions();
+    }
+
+    private void updateUIPositions() {
+        int upperRowHeight = Gdx.graphics.getHeight() - 10;
+
+        // Button Row
+        createMarker.setPosition(10, upperRowHeight, Align.topLeft);
+        createTracker.setPosition(20 + 64,upperRowHeight, Align.topLeft);
     }
 }
